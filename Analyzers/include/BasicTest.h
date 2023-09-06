a0339214559f1a66cb5742388acc5e666cd8cf8e
Junho <soarnsoar@gmail.com>
2023-09-06 14:28:20 +0900

    add ProcessName

---
+++ Analyzers/include/BasicTest.h
@@ -0,0 +1,38 @@
+#ifndef BasicTest_h
+#define BasicTest_h
+
+#include "AnalyzerCore.h"
+
+class BasicTest : public AnalyzerCore {
+
+public:
+
+  void initializeAnalyzer();
+
+  void executeEventFromParameter(AnalyzerParameter param);
+  void executeEvent();
+
+  bool RunSyst;
+  bool RunNewPDF;
+  bool RunXSecSyst;
+
+  TString IsoMuTriggerName;
+  double TriggerSafePtCut;
+
+  vector<TString> MuonIDs, MuonIDSFKeys;
+  vector<Muon> AllMuons;
+  vector<Jet> AllJets;
+
+  double weight_Prefire;
+  //
+  TString ProcessName;
+
+  BasicTest();
+  ~BasicTest();
+
+};
+
+
+
+#endif
+
