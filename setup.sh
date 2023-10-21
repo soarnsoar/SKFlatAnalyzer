9186614015de2cd2877430adfc1d99306e4cfa0c
Junho <soarnsoar@gmail.com>
2023-10-21 23:30:31 +0900

    change cmssw release to CMSSW_10_6_4

--- setup.sh
+++ setup.sh
@@ -10,8 +10,10 @@ export DATA_DIR=$SKFlat_WD/data/$SKFlatV
 #### use cvmfs for root ####
 export CMS_PATH=/cvmfs/cms.cern.ch
 source $CMS_PATH/cmsset_default.sh
-export SCRAM_ARCH=slc7_amd64_gcc900
-export cmsswrel='cmssw/CMSSW_11_2_5'
+#export SCRAM_ARCH=slc7_amd64_gcc900
+export SCRAM_ARCH=slc7_amd64_gcc820
+#export cmsswrel='cmssw/CMSSW_11_2_5'
+export cmsswrel='cmssw/CMSSW_10_6_4'
 cd /cvmfs/cms.cern.ch/$SCRAM_ARCH/cms/$cmsswrel/src
 echo "@@@@ SCRAM_ARCH = "$SCRAM_ARCH
 echo "@@@@ cmsswrel = "$cmsswrel
