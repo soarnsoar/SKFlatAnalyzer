a6ee1bc237b06c507146561f4a6476e2b285008f
Junho <soarnsoar@gmail.com>
2023-11-09 01:25:53 +0900

    overall update 2

--- Analyzers/include/Analyzers_LinkDef.h
+++ Analyzers/include/Analyzers_LinkDef.h
@@ -25,6 +25,7 @@
 #pragma link C++ class B_Info_Analyzer_BASIC_v2309+;
 #pragma link C++ class BasicTest+;
 #pragma link C++ class BBbarRecoTMVA+;
+#pragma link C++ class BBbarRecoTMVA_DEBUG+;
 #pragma link C++ class BBbar_Analyzer+;
 #pragma link C++ class ForTMVA_BBbarAnalyzer+;
 #pragma link C++ class ForTMVA_BBbarAnalyzer_pjetrest0p7_3+;
