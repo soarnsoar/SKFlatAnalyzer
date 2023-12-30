6023e2c2dd5e6746a64e9d4d824caad207993124
Junho <soarnsoar@gmail.com>
2023-12-30 10:14:59 +0900

    mv TMVA to data

---
+++ data/Run2UltraLegacy_v3/2017/TMVA/LepJetChargeReliability/aMCatNLO/cp_models.sh
@@ -0,0 +1,11 @@
+object=muon
+mkdir -p ${object}
+cp /data6/Users/jhchoi/SKFlatAnalyzers/test/SKFlatAnalyzer/keras_my/scripts/JetLepChargeReliability/aMCatNLO/WORKDIR/WORKDIR__5000__500__5__0.3__${object}/TMVAClassification_2017/weights_train_bjet_charge_2017/* ${object}/
+
+object=electron
+mkdir -p ${object}
+cp /data6/Users/jhchoi/SKFlatAnalyzers/test/SKFlatAnalyzer/keras_my/scripts/JetLepChargeReliability/aMCatNLO/WORKDIR/WORKDIR__5000__500__5__0.3__${object}/TMVAClassification_2017/weights_train_bjet_charge_2017/* ${object}/
+
+object=jet
+mkdir -p ${object}
+cp /data6/Users/jhchoi/SKFlatAnalyzers/test/SKFlatAnalyzer/keras_my/scripts/JetLepChargeReliability/aMCatNLO/WORKDIR/WORKDIR__5000__500__5__0.3__${object}/TMVAClassification_2017/weights_train_bjet_charge_2017/* ${object}/
