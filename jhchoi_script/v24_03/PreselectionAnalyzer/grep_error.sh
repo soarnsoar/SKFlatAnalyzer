81b6f0f42158676fa63fd76de2a5c07fe4fab277
jhchoi <soarnsoar@gmail.com>
2026-08-14 20:46:24 +0900

    fix grep_error.sh

--- jhchoi_script/v24_03/PreselectionAnalyzer/grep_error.sh
+++ jhchoi_script/v24_03/PreselectionAnalyzer/grep_error.sh
@@ -3,6 +3,8 @@
 
 #grep -r "err" 201*/ApplyBtagSF_use_beff_JETPUID_L_chi2kincut_syslogs/*.log|grep -v HT|grep ERR
 
-logdir=jetpuid_loose_newlepveto_check_tmva_input_syslogs
-grep -r "rr" 201*/${logdir}/*.log|grep -v HT
-grep -r "RR" 201*/${logdir}/*.log|grep -v HT
+logdirs=(jetpuid_loose_newlepveto_check_tmva_input_syslogs jetpuid_loose_newlepveto_kincutopt_logs)
+for logdir in ${logdirs[@]};do
+    grep -r "rr" 201*/${logdir}/*.log|grep -v HT
+    grep -r "RR" 201*/${logdir}/*.log|grep -v HT
+done
