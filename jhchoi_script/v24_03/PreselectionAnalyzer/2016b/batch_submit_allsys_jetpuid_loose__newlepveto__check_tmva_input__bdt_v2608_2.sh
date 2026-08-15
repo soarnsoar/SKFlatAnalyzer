9736e0f9f775474ae0457156acfb93d0c97cef9f
Junho <soarnsoar@gmail.com>
2026-08-15 09:18:06 +0900

    allsys_jetpuid_loose__newlepveto__check_tmva_input__bdt_v2608_2

---
+++ jhchoi_script/v24_03/PreselectionAnalyzer/2016b/batch_submit_allsys_jetpuid_loose__newlepveto__check_tmva_input__bdt_v2608_2.sh
@@ -0,0 +1,31 @@
+#PreselectionAnalyzer
+mkdir -p jetpuid_loose_newlepveto_check_tmva_input_bdt_v2608_2_syslogs/
+
+SKIM="--skim SkimTree_Dilepton_1DeepJetTightWP"
+FLAG="--userflags runSys"
+FLAG="--userflags runSys,jetpuid_loose,newlepveto,check_tmva_input,bdt_v2608_2"
+NMAX="--nmax 400"
+MEM="--memory 6399 --count 2 "
+YEAR=2016b
+
+
+SKFlat.py -a PreselectionAnalyzer  ${SKIM} -l  inputlist_major.txt -n 50 -e ${YEAR} $FLAG ${NMAX}  ${MEM} &> jetpuid_loose_newlepveto_check_tmva_input_bdt_v2608_2_syslogs/major.log&
+SKFlat.py -a PreselectionAnalyzer  ${SKIM} -l  inputlist_TT.txt -n 150 -e ${YEAR} $FLAG ${NMAX}  ${MEM} &> jetpuid_loose_newlepveto_check_tmva_input_bdt_v2608_2_syslogs/TT.log&
+MEM="--memory 3199 --count 1"
+
+SKFlat.py -a PreselectionAnalyzer  ${SKIM} -l inputlist_minor.txt -n 50 -e ${YEAR} $FLAG ${NMAX}  ${MEM} &> jetpuid_loose_newlepveto_check_tmva_input_bdt_v2608_2_syslogs/minor.log&
+SKFlat.py -a PreselectionAnalyzer  ${SKIM} -l inputlist_tW.txt -n 50 -e ${YEAR} $FLAG ${NMAX}  ${MEM} &> jetpuid_loose_newlepveto_check_tmva_input_bdt_v2608_2_syslogs/tW.log&
+SKFlat.py -a PreselectionAnalyzer  ${SKIM} -i DoubleMuon -n 50 -e ${YEAR} $FLAG ${NMAX}  ${MEM} &> jetpuid_loose_newlepveto_check_tmva_input_bdt_v2608_2_syslogs/DoubleMuon.log&
+SKFlat.py -a PreselectionAnalyzer  ${SKIM} -i DoubleEG -n 50 -e ${YEAR} $FLAG ${NMAX}  ${MEM} &> jetpuid_loose_newlepveto_check_tmva_input_bdt_v2608_2_syslogs/DoubleEG.log&
+SKFlat.py -a PreselectionAnalyzer  ${SKIM} -l inputlist_NoSkim.txt -n 50 -e ${YEAR} $FLAG ${NMAX}  ${MEM} &> jetpuid_loose_newlepveto_check_tmva_input_bdt_v2608_2_syslogs/NoSkim.log&
+SKFlat.py -a PreselectionAnalyzer  ${SKIM} -l  inputlist_VV.txt -n 50 -e ${YEAR} $FLAG ${NMAX}  ${MEM} &> jetpuid_loose_newlepveto_check_tmva_input_bdt_v2608_2_syslogs/VV.log&
+
+
+
+#SKFlat.py -a PreselectionAnalyzer  ${SKIM} -l  inputlist_major.txt -n 50 -e ${YEAR} $FLAG ${NMAX}  ${MEM} &> jetpuid_loose_newlepveto_check_tmva_input_bdt_v2608_2_syslogs/major.log&
+#SKFlat.py -a PreselectionAnalyzer  ${SKIM} -l inputlist_minor.txt -n 50 -e ${YEAR} $FLAG ${NMAX}  ${MEM} &> jetpuid_loose_newlepveto_check_tmva_input_bdt_v2608_2_syslogs/minor.log&
+#SKFlat.py -a PreselectionAnalyzer  ${SKIM} -l inputlist_tW.txt -n 50 -e ${YEAR} $FLAG ${NMAX}  ${MEM} &> jetpuid_loose_newlepveto_check_tmva_input_bdt_v2608_2_syslogs/tW.log&
+#SKFlat.py -a PreselectionAnalyzer  ${SKIM} -i DoubleMuon -n 50 -e ${YEAR} $FLAG ${NMAX}  ${MEM} &> jetpuid_loose_newlepveto_check_tmva_input_bdt_v2608_2_syslogs/DoubleMuon.log&
+#SKFlat.py -a PreselectionAnalyzer  ${SKIM} -i DoubleEG -n 50 -e ${YEAR} $FLAG ${NMAX}  ${MEM} &> jetpuid_loose_newlepveto_check_tmva_input_bdt_v2608_2_syslogs/DoubleEG.log&
+#SKFlat.py -a PreselectionAnalyzer  ${SKIM} -l inputlist_NoSkim.txt -n 50 -e ${YEAR} $FLAG ${NMAX}  ${MEM} &> jetpuid_loose_newlepveto_check_tmva_input_bdt_v2608_2_syslogs/NoSkim.log&
+#SKFlat.py -a PreselectionAnalyzer  ${SKIM} -l  inputlist_VV.txt -n 50 -e ${YEAR} $FLAG ${NMAX}  ${MEM} &> jetpuid_loose_newlepveto_check_tmva_input_bdt_v2608_2_syslogs/VV.log&
--- jhchoi_script/v24_03/PreselectionAnalyzer/grep_error.sh
+++ jhchoi_script/v24_03/PreselectionAnalyzer/grep_error.sh
@@ -3,7 +3,7 @@
 
 #grep -r "err" 201*/ApplyBtagSF_use_beff_JETPUID_L_chi2kincut_syslogs/*.log|grep -v HT|grep ERR
 
-logdirs=(jetpuid_loose_newlepveto_check_tmva_input_syslogs jetpuid_loose_newlepveto_kincutopt_logs)
+logdirs=(jetpuid_loose_newlepveto_check_tmva_input_syslogs jetpuid_loose_newlepveto_kincutopt_logs jetpuid_loose_newlepveto_check_tmva_input_bdt_v2608_2_syslogs)
 for logdir in ${logdirs[@]};do
     grep -r "rr" 201*/${logdir}/*.log|grep -v HT
     grep -r "RR" 201*/${logdir}/*.log|grep -v HT
