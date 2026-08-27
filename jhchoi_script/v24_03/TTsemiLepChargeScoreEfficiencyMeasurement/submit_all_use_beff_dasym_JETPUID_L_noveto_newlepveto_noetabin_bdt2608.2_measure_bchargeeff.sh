a9e5bb54a68273ab19401a8054270d71ff0e296a
jhchoi <soarnsoar@gmail.com>
2026-08-27 16:32:56 +0900

    TTsemiLepChargeScoreEfficiencyMeasurement submission

---
+++ jhchoi_script/v24_03/TTsemiLepChargeScoreEfficiencyMeasurement/submit_all_use_beff_dasym_JETPUID_L_noveto_newlepveto_noetabin_bdt2608.2_measure_bchargeeff.sh
@@ -0,0 +1,8 @@
+ARR_DIR=(2017 2016a 2016b 2018)
+for D in ${ARR_DIR[@]};do
+    echo ${D}
+    cd ${D}
+    source batch_submit_all_use_beff_dasym_JETPUID_L_noveto_newlepveto_noetabin_bdt2608.2_measure_bchargeeff.sh
+    sleep 60
+    cd -
+done
