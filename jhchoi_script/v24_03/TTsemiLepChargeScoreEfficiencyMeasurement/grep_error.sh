ababbe333933bfc0a30e8222dbafca9f453279e8
jhchoi <soarnsoar@gmail.com>
2026-08-27 16:44:59 +0900

    fix grep_error.sh at TTsemiLepChargeScoreEfficiencyMeasurement

--- jhchoi_script/v24_03/TTsemiLepChargeScoreEfficiencyMeasurement/grep_error.sh
+++ jhchoi_script/v24_03/TTsemiLepChargeScoreEfficiencyMeasurement/grep_error.sh
@@ -3,8 +3,7 @@
 
 #grep -r "err" 201*/ApplyBtagSF_use_beff_JETPUID_L_chi2kincut_syslogs/*.log|grep -v HT|grep ERR
 
-logdir=use_beff_dasym_JETPUID_L_chi2kincut_bdt2512.5_splitcharge_noetabin_syslogs
-grep -r "err" 201*/${logdir}/*.log|grep -v HT|grep err
-grep -r "err" 201*/${logdir}/*.log|grep -v HT|grep Err
-
-grep -r "err" 201*/${logdir}/*.log|grep -v HT|grep ERR
+logdirs=(use_beff_dasym_JETPUID_L_noveto_newlepveto_noetabin_bdt2608.2_measure_bchargeeff_logs)
+for logdir in ${logdirs[@]};do
+    grep -r "rr" 201*/${logdir}/*.log|grep -v HT
+done
