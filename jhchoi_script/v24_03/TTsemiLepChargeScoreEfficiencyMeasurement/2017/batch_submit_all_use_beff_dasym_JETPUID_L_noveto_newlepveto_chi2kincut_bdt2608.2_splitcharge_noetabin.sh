e7b08abae0fd9b03c1da6490291ce7c8fa604126
jhchoi <soarnsoar@gmail.com>
2026-08-29 05:28:54 +0900

    TTsemiLepChargeScoreEfficiencyMeasurement nosys

---
+++ jhchoi_script/v24_03/TTsemiLepChargeScoreEfficiencyMeasurement/2017/batch_submit_all_use_beff_dasym_JETPUID_L_noveto_newlepveto_chi2kincut_bdt2608.2_splitcharge_noetabin.sh
@@ -0,0 +1,57 @@
+mkdir -p _use_beff_dasym_JETPUID_L_noveto_newlepveto_chi2kincut_bdt2608.2_splitcharge_noetabin_logs/
+
+FLAG="--userflags use_beff_dasym,JETPUID_L,noveto,newlepveto,chi2kincut,bdt2608.2,splitcharge,noetabin"
+SKIM="--skim SkimTree_SingleLepton_1DeepJetTightWP"
+
+
+YEAR=2017
+MAXJOB=" --nmax 800 "
+
+
+MEM_TT="--memory 6400" ##weight=3 -> 120
+C_TT="--count 3"
+MEM_DY="--memory 6300" ## weight=2 -> 20
+C_DY="--count 3"
+MEM_tW="--memory 6300"
+C_tW="--count 3"
+MEM_ST="--memory 6300"
+C_ST="--count 3"
+
+##--TTLJ
+NJOB=150
+if [[ "$HOSTNAME" == *knu* ]]; then
+    NJOB=300
+fi
+SKFlat.py -a TTsemiLepChargeScoreEfficiencyMeasurement  ${SKIM} -i TTLJ_powheg -n ${NJOB} -e ${YEAR} $FLAG ${MAXJOB} ${MEM_TT} ${C_TT}&> _use_beff_dasym_JETPUID_L_noveto_newlepveto_chi2kincut_bdt2608.2_splitcharge_noetabin_logs/TTLJ_powheg.log&
+sleep 15
+
+##---TTLL
+NJOB=50
+if [[ "$HOSTNAME" == *knu* ]]; then
+    NJOB=300
+fi
+SKFlat.py -a TTsemiLepChargeScoreEfficiencyMeasurement  ${SKIM} -i TTLL_powheg -n ${NJOB} -e ${YEAR} $FLAG ${MAXJOB} ${MEM_TT} ${C_TT} &> _use_beff_dasym_JETPUID_L_noveto_newlepveto_chi2kincut_bdt2608.2_splitcharge_noetabin_logs/TTLL_powheg.log&
+
+
+sleep 15
+##--DY
+SKFlat.py -a TTsemiLepChargeScoreEfficiencyMeasurement  ${SKIM} -l  inputlist_DY.txt -n 40 -e ${YEAR} $FLAG ${MAXJOB} ${MEM_DY} ${C_DY} &> _use_beff_dasym_JETPUID_L_noveto_newlepveto_chi2kincut_bdt2608.2_splitcharge_noetabin_logs/DY.log&
+sleep 15
+##--ST
+SKFlat.py -a TTsemiLepChargeScoreEfficiencyMeasurement  ${SKIM} -l inputlist_ST.txt -n 40 -e ${YEAR} $FLAG ${MAXJOB} ${MEM_ST} ${C_ST} &> _use_beff_dasym_JETPUID_L_noveto_newlepveto_chi2kincut_bdt2608.2_splitcharge_noetabin_logs/ST.log&
+sleep 15
+##--tW
+SKFlat.py -a TTsemiLepChargeScoreEfficiencyMeasurement  ${SKIM} -l inputlist_tW.txt -n 40 -e ${YEAR} $FLAG ${MAXJOB} ${MEM_tW} ${C_tW} &> _use_beff_dasym_JETPUID_L_noveto_newlepveto_chi2kincut_bdt2608.2_splitcharge_noetabin_logs/tW.log&
+sleep 15
+
+
+MEM="--memory 3200 --count 2"
+SKFlat.py -a TTsemiLepChargeScoreEfficiencyMeasurement  ${SKIM} -l inputlist_minor.txt -n 30 -e ${YEAR} $FLAG ${MAXJOB} ${MEM} &> _use_beff_dasym_JETPUID_L_noveto_newlepveto_chi2kincut_bdt2608.2_splitcharge_noetabin_logs/minor.log&
+sleep 15
+SKFlat.py -a TTsemiLepChargeScoreEfficiencyMeasurement ${SKIM} -l inputlist_NoSkim.txt -n 30 -e ${YEAR} $FLAG ${MAXJOB}  ${MEM} &> _use_beff_dasym_JETPUID_L_noveto_newlepveto_chi2kincut_bdt2608.2_splitcharge_noetabin_logs/NoSkim.log&
+sleep 15
+SKFlat.py -a TTsemiLepChargeScoreEfficiencyMeasurement  ${SKIM} -i SingleMuon -n 50 -e ${YEAR} $FLAG ${MAXJOB} ${MEM}  &> _use_beff_dasym_JETPUID_L_noveto_newlepveto_chi2kincut_bdt2608.2_splitcharge_noetabin_logs/SingleMuon.log&
+sleep 15
+SKFlat.py -a TTsemiLepChargeScoreEfficiencyMeasurement  ${SKIM} -i SingleElectron -n 50 -e ${YEAR} $FLAG ${MAXJOB} ${MEM}  &> _use_beff_dasym_JETPUID_L_noveto_newlepveto_chi2kincut_bdt2608.2_splitcharge_noetabin_logs/SingleElectron.log&
+sleep 15
+
