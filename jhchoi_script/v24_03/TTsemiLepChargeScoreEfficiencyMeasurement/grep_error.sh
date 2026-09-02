cd82d641a53071b2588ad61d524550f85ec10422
jhchoi <soarnsoar@gmail.com>
2026-09-03 08:42:53 +0900

    submit_all_use_beff_dasym_JETPUID_L_noveto_newlepveto_chi2kincut_bdt2608.2_splitcharge_noetabin_measure_bchargeeff.sh

--- jhchoi_script/v24_03/TTsemiLepChargeScoreEfficiencyMeasurement/grep_error.sh
+++ jhchoi_script/v24_03/TTsemiLepChargeScoreEfficiencyMeasurement/grep_error.sh
@@ -3,7 +3,10 @@
 
 #grep -r "err" 201*/ApplyBtagSF_use_beff_JETPUID_L_chi2kincut_syslogs/*.log|grep -v HT|grep ERR
 
-logdirs=(use_beff_dasym_JETPUID_L_noveto_newlepveto_noetabin_bdt2608.2_measure_bchargeeff_logs)
+logdirs=(
+    use_beff_dasym_JETPUID_L_noveto_newlepveto_chi2kincut_bdt2608.2_splitcharge_noetabin_measure_bchargeeff_logs
+    use_beff_dasym_JETPUID_L_noveto_newlepveto_noetabin_bdt2608.2_measure_btageff_tight_logs
+)
 for logdir in ${logdirs[@]};do
     grep -r "rr" 201*/${logdir}/*.log|grep -v HT
 done
