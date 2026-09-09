7dcc3de1f06183734b95ab906a5147948c19f7a6
Junho <soarnsoar@gmail.com>
2026-09-10 08:31:54 +0900

    2018_bchargeideff_test

---
+++ jhchoi_script/v24_03/PreselectionAnalyzer/2018_bchargeideff_test/batch_submit_all_jetpuid_loose__newlepveto__use_beffasym__bdt_v2608_2__bdtcut__measure_bchargeeff_pveto_nody1b.sh
@@ -0,0 +1,25 @@
+#PreselectionAnalyzer
+mkdir -p jetpuid_loose_newlepveto_use_beffasym_bdt_v2608_2_bdtcut_measure_bchargeeff_pveto_nody1b_logs/
+
+SKIM="--skim SkimTree_Dilepton_1DeepJetTightWP"
+FLAG="--userflags jetpuid_loose,newlepveto,use_beffasym,bdt_v2608_2,bdtcut,measure_bchargeeff_pveto,nody1b"
+NMAX="--nmax 400"
+
+YEAR=2018
+SKFlat.py -a PreselectionAnalyzer  ${SKIM} -l  inputlist_major.txt -n 50 -e ${YEAR} $FLAG ${NMAX} &> jetpuid_loose_newlepveto_use_beffasym_bdt_v2608_2_bdtcut_measure_bchargeeff_pveto_nody1b_logs/major.log&
+sleep 5
+SKFlat.py -a PreselectionAnalyzer  ${SKIM} -l  inputlist_TT.txt -n 150 -e ${YEAR} $FLAG ${NMAX} &> jetpuid_loose_newlepveto_use_beffasym_bdt_v2608_2_bdtcut_measure_bchargeeff_pveto_nody1b_logs/TT.log&
+sleep 5
+SKFlat.py -a PreselectionAnalyzer  ${SKIM} -l inputlist_minor.txt -n 50 -e ${YEAR} $FLAG ${NMAX} &> jetpuid_loose_newlepveto_use_beffasym_bdt_v2608_2_bdtcut_measure_bchargeeff_pveto_nody1b_logs/minor.log&
+sleep 5
+SKFlat.py -a PreselectionAnalyzer  ${SKIM} -l inputlist_tW.txt -n 50 -e ${YEAR} $FLAG ${NMAX} &> jetpuid_loose_newlepveto_use_beffasym_bdt_v2608_2_bdtcut_measure_bchargeeff_pveto_nody1b_logs/tW.log&
+
+#SKFlat.py -a PreselectionAnalyzer  ${SKIM} -i DoubleMuon -n 50 -e ${YEAR} $FLAG ${NMAX} &> jetpuid_loose_newlepveto_use_beffasym_bdt_v2608_2_bdtcut_measure_bchargeeff_pveto_nody1b_logs/DoubleMuon.log&
+#SKFlat.py -a PreselectionAnalyzer  ${SKIM} -i EGamma -n 50 -e ${YEAR} $FLAG ${NMAX} &> jetpuid_loose_newlepveto_use_beffasym_bdt_v2608_2_bdtcut_measure_bchargeeff_pveto_nody1b_logs/EGamma.log&
+sleep 5
+SKFlat.py -a PreselectionAnalyzer ${SKIM} -l inputlist_NoSkim.txt -n 50 -e ${YEAR} $FLAG ${NMAX} &> jetpuid_loose_newlepveto_use_beffasym_bdt_v2608_2_bdtcut_measure_bchargeeff_pveto_nody1b_logs/NoSkim.log&
+sleep 5
+SKFlat.py -a PreselectionAnalyzer  ${SKIM} -l  inputlist_VV.txt -n 50 -e ${YEAR} $FLAG ${NMAX} &> jetpuid_loose_newlepveto_use_beffasym_bdt_v2608_2_bdtcut_measure_bchargeeff_pveto_nody1b_logs/VV.log&
+
+
+
---
+++ jhchoi_script/v24_03/PreselectionAnalyzer/2018_bchargeideff_test/grep_error.sh
@@ -0,0 +1,4 @@
+
+grep -r "rr" *measure_bchargeeff*_logs/*.log|grep -v HT
+grep -r "RR" *measure_bchargeeff*_logs/*.log|grep -v HT
+
